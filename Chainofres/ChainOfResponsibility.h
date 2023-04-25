df4cc21646ec9e112021f9c4573811f053035260
ArtemShchuchev <shuaa@rambler.ru>
2023-04-25 14:35:02 +0700

    пример из презентации

--- Chainofres/ChainOfResponsibility.h
+++ Chainofres/ChainOfResponsibility.h
@@ -9,10 +9,60 @@
 #include <thread>
 
 #include "SecondaryFunction.h"
-
+/*
 class LogMessage
 {
 public:
     //Type type() const;
     const std::string& message() const;
+};
+*/
+
+class View // Базовый класс окошка, которое видит пользователь
+{
+public:
+    View(int x, int y, int width, int height) // Кординаты позиции на экране и размер окошка
+        : x_(x), y_(y), width_(width), height_(height) {}
+    virtual ~View() = default;
+    
+    bool containsClick(int x, int y) const // Определяем пришёлся ли клик в окошко
+    {
+        return x >= x_ && x <= x_ + width_ && y >= y_ && y <= y_ + height_;
+    }
+    
+    void addSubview(std::unique_ptr<View> view) // Встраиваем другое окошко поверх текущего
+    {
+        view->parent_ = this;
+        subviews_.push_back(std::move(view));
+    }
+    
+    void handleClick(int x, int y)
+    {
+        if (containsClick(x, y))
+        {
+            std::wcout << L"Clicked view " << this << L" at " << x << L":" << y << std::endl;
+            onClick(); // Обрабатываем клик если он пришёлся в это окошко
+            return;
+        }
+        if (parent_) parent_->handleClick(x, y); // Иначе пробрасываем клик в родительское окошко
+    }
+    virtual void onClick() {} // Обработка клика переопределяется в классах-наследниках
+
+private:
+    std::vector<std::unique_ptr<View>> subviews_;
+    View* parent_ = nullptr;
+    int x_ = 0;
+    int y_ = 0;
+    int width_ = 0;
+    int height_ = 0;
+};
+
+class Button : public View
+{
+public:
+    Button(int x, int y, int width, int height) : View(x, y, width, height) {}
+    void onClick() override
+    {
+        std::wcout << L"Clicked button " << this << std::endl;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -46,7 +46,12 @@ int main(int argc, char** argv)
 {
 	printHeader(L"Задание 3. Паттерн «Цепочка ответственности»");
 
-	
+	auto root = std::make_unique<View>(0, 0, 256, 256); // Создаём основное окошко
+	auto button = std::make_unique<Button>(50, 50, 25, 25); // Создаём кнопку
+	auto* foregroundView = button.get(); // Окошко перед пользователем
+	root->addSubview(std::move(button)); // Встраиваем кнопку в основное окошко
+	foregroundView->handleClick(56, 70); // Кликаем в кнопку
+	foregroundView->handleClick(160, 130); // Кликаем мимо кнопки
 	
 	std::wcout << "\n";
 	return 0;

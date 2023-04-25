c513871d178c06207573aa7f16f737ea3142caf6
ArtemShchuchev <shuaa@rambler.ru>
2023-04-25 14:22:55 +0700

    Добавьте файлы проекта.

---
+++ Chainofres/ChainOfResponsibility.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <thread>
+
+#include "SecondaryFunction.h"
+
+class LogMessage
+{
+public:
+    //Type type() const;
+    const std::string& message() const;
+};
